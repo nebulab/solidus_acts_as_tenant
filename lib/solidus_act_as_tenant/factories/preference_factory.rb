40e6968a04c1891b4992ab645d1973a1ae8cf819
Ikraam Ghoor <ikraamghoor@gmail.com>
2025-02-19 17:35:00 +0000

    Fix rubocop

--- lib/solidus_act_as_tenant/factories/preference_factory.rb
+++ lib/solidus_act_as_tenant/factories/preference_factory.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :preference, class: Spree::Preference do
+  factory :preference, class: 'Spree::Preference' do
     sequence(:key) { |i| "Key#{i}" }
   end
 end
---
+++ spec/lib/solidus_act_as_tenant/solidus_act_as_tenant_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+RSpec.describe SolidusActAsTenant do
+  describe '.configuration' do
+    it 'returns the same configuration instance' do
+      expect(described_class.configuration).to eq(described_class.configuration)
+    end
+
+    it 'aliases config to configuration' do
+      expect(described_class.config).to eq(described_class.configuration)
+    end
+  end
+
+  describe '.configure' do
+    it 'yields the configuration instance' do
+      expect { |b| described_class.configure(&b) }.to yield_with_args(SolidusActAsTenant::Configuration)
+    end
+
+    it 'allows configuring through a block' do
+      described_class.configure do |config|
+        config.tenant_column_name = :custom_tenant_id
+      end
+
+      expect(described_class.configuration.tenant_column_name).to eq(:custom_tenant_id)
+    end
+  end
+end
